52a83d3816a99da03cc548e81457287795540a3a
liqi <liqi@douyu.tv>
2018-10-09 12:42:08 +0800

    fix

--- runner/build.go
+++ runner/build.go
@@ -10,7 +10,7 @@ import (
 func build() (string, bool) {
 	buildLog("Building...")
 
-	cmd := exec.Command("go", "build", "-o", buildPath()})
+	cmd := exec.Command("go", "build", "-o", buildPath())
 
 	stderr, err := cmd.StderrPipe()
 	if err != nil {
